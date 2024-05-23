54c12f720c7d36e2ae1e63569add997d47acbbe5
BrettFraley <brettfraley@proton.me>
2024-05-22 21:10:12 -0400

    Add setup for level tests, the levelLooper function and setup to begin building out a level 1 of gameplay

---
+++ levels/levelTests.js
@@ -0,0 +1,14 @@
+export const TESTS = {
+
+  INTRO_LEVEL_TEST: [
+    "Level 1 Test",
+    "Level 1 Test 2",
+    "Level 1 Test 3"
+  ],
+
+  LEVEL_TWO_TEST: [
+    "Level 2 Test",
+    "Level 2 Test 2",
+    "Level 2 Test 3"
+  ]
+}
--- syntax-blast.js
+++ syntax-blast.js
@@ -1,40 +1,48 @@
+import { TESTS } from "/levels/levelTests.js";
+
+
 const SyntaxBlast = config => {
 
+    // Wrappers and UI controls
     let levelArea = document.getElementById('level-area')
     let playerStats = document.getElementById('player-stats')
     let startButton = document.getElementById('start-button')
 
+    // Game level cahllenge elements
+    let currentChallenge = document.getElementById('current-challenge')
+
+
+    // Keyboard input
+    let playerInput = document.getElementById('player-input')
+
+
     startButton.addEventListener('click', () => {
-
-        gameInit() // <- take levels, series', mode? etc
-                
-        // note when coming back here... things should be like
-        // series.start, series.pause, series.stop, series.complete
-
+        gameInit()
     }, false)
 
-    setEl("level-area", "SET LEVEL AREA TEST")
     setEl("player-stats", "SET PLAYER STATS TEST")
+
+    function levelLooper() {
+        let i = 0
+        setInterval(() => {
+
+            i = i == TESTS.INTRO_LEVEL_TEST.length - 1 ? 0 : i + 1
+
+            currentChallenge.innerHTML = TESTS.INTRO_LEVEL_TEST[i]
+
+      }, 3000);
+    } levelLooper();
 }
 
-// This needs to work in a few ways.. basically need top create and
-// append elements of various types that will contain various types
-// values... mainly innerText values.
 const setEl = (id, val) => document.getElementById(id).innerHTML = val
 
-
+// NOTE: gameInit shout accept params like
+// series.start, series.pause, series.stop, series.complete
 const gameInit = () => {
-    console.log('testSeries called on gameStart')
-    
+   SyntaxBlast('test - config')
 }
 
-const LEVEL_1_BLOCKS = [
-    { time_limit : 10000 },
-    "Hello World!",
-    "Welcome to S y N T a X B l A s T"
-]
 
-SyntaxBlast('test - config')
 
 /**
 Give the illusion of paralax proportion using
